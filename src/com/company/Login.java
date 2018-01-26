d916770ac88a3d29c9b32b094a21d5dc8c31e48c
Luke Mwila <outlier.developer@gmail.com>
2018-01-26 22:24:37 +0200

    Initial push

---
+++ src/com/company/Login.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JPanel implements ActionListener{
+    JLabel userL = new JLabel("Username: ");
+    JTextField userTF = new JTextField();
+    
+    Login(){
+
+    }
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
